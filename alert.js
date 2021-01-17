d2315e6a263d4ed03f8d97c60a8d65c844abfcfe
MaksimTsvik <mkdemix@tut.by>
2021-01-17 23:16:54 +0300

    next

--- alert.js
+++ alert.js
@@ -1,11 +1,6 @@
 "use strict";
-let admin;
-const name = 'Джон';
-
-admin = name;
-
-alert(admin);
-alert(name + '2nd');
+const ourPlanet = 'Earth';
+let currentVisitor;
 
 
 alert("Я JavaScript!")
--- readme.txt
+++ readme.txt
@@ -1,1 +1,3 @@
 Hello! My name is Maks.
+This folder is used to practice in JS basics.
+learn.javascript.ru
