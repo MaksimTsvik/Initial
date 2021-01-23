24a14a0b8bdebd8aeee9ab1928c233db722c911f
MaksimTsvik <mkdemix@tut.by>
2021-01-23 17:29:09 +0300

    last

--- alert.js
+++ alert.js
@@ -5,3 +5,21 @@ function pow(x, n) {
     if (Math.round(n) != n) return NaN;
     return Math.pow(x, n);
 }
+
+let ladder = {
+    step: 0,
+    up() {
+        this.step++;
+        return this;
+    },
+    down() {
+        this.step--;
+        return this;
+    },
+    showStep() {
+        alert(this.step);
+        return this;
+    }
+}
+
+ladder.up().up().down().up().down().showStep();
