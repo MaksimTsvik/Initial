794f1c8d7d276919667a92644adce3a71155dc23
MaksimTsvik <mkdemix@tut.by>
2021-01-26 20:11:25 +0300

    Updated

--- alert.js
+++ alert.js
@@ -1,21 +1,8 @@
 "use strict";
 
-function pow(x, n) {
-    if (n < 0) return NaN;
-    if (Math.round(n) != n) return NaN;
-    return Math.pow(x, n);
+function removeEveryOther(arr) {
+    for (let i = 1; i < arr.length; i + 2) {
+        arr.splice([i], 1);
+    }
+    return arr;
 }
-
-function Accumulator(startingValue) {
-    this.value = startingValue;
-    this.read = function () {
-        this.value += +prompt('Type a number', '');
-    };
-}
-
-let accumulator = new Accumulator(1); // начальное значение 1
-
-accumulator.read(); // прибавит ввод prompt к текущему значению
-accumulator.read(); // прибавит ввод prompt к текущему значению
-
-alert(accumulator.value); // выведет сумму этих значений
